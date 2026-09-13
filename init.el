44935a7c449e933b484aa5830ed7fe7dd239e224
Glocean <211661684+gloceansh@users.noreply.github.com>
2026-09-13 16:15:03 +0400

    switch lsp-ui-sideline-show-hover off

--- init.el
+++ init.el
@@ -632,7 +632,7 @@
   (lsp-ui-doc-delay 0.5)
   (lsp-ui-doc-position 'at-point)
   (lsp-ui-sideline-show-diagnostics t)
-  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-sideline-show-hover nil)
   (lsp-ui-sideline-show-code-actions t))
 
 ;; -----------------------------------------------------------------------------
